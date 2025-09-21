812573dbe896273e30710fdf3f7517b6aaeb122a
Vedant Jaimin Gandhi <138110993+VeduStorm@users.noreply.github.com>
2025-09-21 18:10:29 +0530

    Delete src directory

--- src/data/framework/liveStatusLoader.ts
+++
@@ -1,6 +0,0 @@
-import {opendiscord, api, utilities} from "../../index"
-
-export const loadAllLiveStatusSources = async () => {
-    //DEFAULT DJDJ DEV
-    opendiscord.livestatus.add(new api.ODLiveStatusUrlSource("opendiscord:default-djdj-dev","https://raw.githubusercontent.com/open-discord-bots/open-ticket/refs/heads/dev/src/livestatus.json"))
-}
